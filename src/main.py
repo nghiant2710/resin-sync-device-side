cc65e2e35f3a826422083da7859e712c16f7d8a8
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-09-25 14:35:18 +0100

    added readme

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
-from time import sleep
-print "Starting main python process"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
